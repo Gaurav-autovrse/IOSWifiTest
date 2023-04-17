7571e423bb60c1562eafe9be795bd934ff449409
Gaurav-autovrse <gaurav.muglikar@autovrse.in>
2023-04-17 11:53:29 +0530

    first commit

---
+++ Build/Classes/Native/Assembly-CSharp.cpp
@@ -0,0 +1,239 @@
+﻿#include "pch-cpp.hpp"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include <limits>
+
+
+
+// IOSBridgeTester
+struct IOSBridgeTester_t50928DFB95A193CA8D2314BF665DF15D2A437ABE;
+// UnityEngine.MonoBehaviour
+struct MonoBehaviour_t532A11E69716D348D8AA7F854AFCBFCB8AD17F71;
+// System.String
+struct String_t;
+// System.Void
+struct Void_t4861ACF8F4594C3437BB48B6E56783494B843915;
+
+IL2CPP_EXTERN_C String_t* _stringLiteralE87E01CC027DFCFE2D69045A36DFD65E43CE6D57;
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// <Module>
+struct U3CModuleU3E_tBB65183F1134474D09FF49B95625D25472B9BA8B 
+{
+};
+
+// System.String
+struct String_t  : public RuntimeObject
+{
+	// System.Int32 System.String::_stringLength
+	int32_t ____stringLength_4;
+	// System.Char System.String::_firstChar
+	Il2CppChar ____firstChar_5;
+};
+
+// System.ValueType
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F  : public RuntimeObject
+{
+};
+// Native definition for P/Invoke marshalling of System.ValueType
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F_marshaled_pinvoke
+{
+};
+// Native definition for COM marshalling of System.ValueType
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F_marshaled_com
+{
+};
+
+// System.IntPtr
+struct IntPtr_t 
+{
+	// System.Void* System.IntPtr::m_value
+	void* ___m_value_0;
+};
+
+// System.Void
+struct Void_t4861ACF8F4594C3437BB48B6E56783494B843915 
+{
+	union
+	{
+		struct
+		{
+		};
+		uint8_t Void_t4861ACF8F4594C3437BB48B6E56783494B843915__padding[1];
+	};
+};
+
+// UnityEngine.Object
+struct Object_tC12DECB6760A7F2CBF65D9DCF18D044C2D97152C  : public RuntimeObject
+{
+	// System.IntPtr UnityEngine.Object::m_CachedPtr
+	intptr_t ___m_CachedPtr_0;
+};
+// Native definition for P/Invoke marshalling of UnityEngine.Object
+struct Object_tC12DECB6760A7F2CBF65D9DCF18D044C2D97152C_marshaled_pinvoke
+{
+	intptr_t ___m_CachedPtr_0;
+};
+// Native definition for COM marshalling of UnityEngine.Object
+struct Object_tC12DECB6760A7F2CBF65D9DCF18D044C2D97152C_marshaled_com
+{
+	intptr_t ___m_CachedPtr_0;
+};
+
+// UnityEngine.Component
+struct Component_t39FBE53E5EFCF4409111FB22C15FF73717632EC3  : public Object_tC12DECB6760A7F2CBF65D9DCF18D044C2D97152C
+{
+};
+
+// UnityEngine.Behaviour
+struct Behaviour_t01970CFBBA658497AE30F311C447DB0440BAB7FA  : public Component_t39FBE53E5EFCF4409111FB22C15FF73717632EC3
+{
+};
+
+// UnityEngine.MonoBehaviour
+struct MonoBehaviour_t532A11E69716D348D8AA7F854AFCBFCB8AD17F71  : public Behaviour_t01970CFBBA658497AE30F311C447DB0440BAB7FA
+{
+};
+
+// IOSBridgeTester
+struct IOSBridgeTester_t50928DFB95A193CA8D2314BF665DF15D2A437ABE  : public MonoBehaviour_t532A11E69716D348D8AA7F854AFCBFCB8AD17F71
+{
+};
+
+// <Module>
+
+// <Module>
+
+// System.String
+struct String_t_StaticFields
+{
+	// System.String System.String::Empty
+	String_t* ___Empty_6;
+};
+
+// System.String
+
+// System.ValueType
+
+// System.ValueType
+
+// System.IntPtr
+struct IntPtr_t_StaticFields
+{
+	// System.IntPtr System.IntPtr::Zero
+	intptr_t ___Zero_1;
+};
+
+// System.IntPtr
+
+// System.Void
+
+// System.Void
+
+// UnityEngine.Object
+struct Object_tC12DECB6760A7F2CBF65D9DCF18D044C2D97152C_StaticFields
+{
+	// System.Int32 UnityEngine.Object::OffsetOfInstanceIDInCPlusPlusObject
+	int32_t ___OffsetOfInstanceIDInCPlusPlusObject_1;
+};
+
+// UnityEngine.Object
+
+// UnityEngine.Component
+
+// UnityEngine.Component
+
+// UnityEngine.Behaviour
+
+// UnityEngine.Behaviour
+
+// UnityEngine.MonoBehaviour
+
+// UnityEngine.MonoBehaviour
+
+// IOSBridgeTester
+
+// IOSBridgeTester
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+
+
+// System.Void IOSBridgeTester::IOSWifiDetails_runNativeCode(System.String)
+IL2CPP_EXTERN_C IL2CPP_METHOD_ATTR void IOSBridgeTester_IOSWifiDetails_runNativeCode_mBE4F9DD95610BB6F54978D965676DC521AB21162 (String_t* ___0_input, const RuntimeMethod* method) ;
+// System.Void UnityEngine.MonoBehaviour::.ctor()
+IL2CPP_EXTERN_C IL2CPP_METHOD_ATTR void MonoBehaviour__ctor_m592DB0105CA0BC97AA1C5F4AD27B12D68A3B7C1E (MonoBehaviour_t532A11E69716D348D8AA7F854AFCBFCB8AD17F71* __this, const RuntimeMethod* method) ;
+IL2CPP_EXTERN_C void DEFAULT_CALL IOSWifiDetails_runNativeCode(char*);
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+// System.Void IOSBridgeTester::IOSWifiDetails_runNativeCode(System.String)
+IL2CPP_EXTERN_C IL2CPP_METHOD_ATTR void IOSBridgeTester_IOSWifiDetails_runNativeCode_mBE4F9DD95610BB6F54978D965676DC521AB21162 (String_t* ___0_input, const RuntimeMethod* method) 
+{
+	typedef void (DEFAULT_CALL *PInvokeFunc) (char*);
+
+	// Marshaling of parameter '___0_input' to native representation
+	char* ____0_input_marshaled = NULL;
+	____0_input_marshaled = il2cpp_codegen_marshal_string(___0_input);
+
+	// Native function invocation
+	reinterpret_cast<PInvokeFunc>(IOSWifiDetails_runNativeCode)(____0_input_marshaled);
+
+	// Marshaling cleanup of parameter '___0_input' native representation
+	il2cpp_codegen_marshal_free(____0_input_marshaled);
+	____0_input_marshaled = NULL;
+
+}
+// System.Void IOSBridgeTester::Start()
+IL2CPP_EXTERN_C IL2CPP_METHOD_ATTR void IOSBridgeTester_Start_m52FB8350EC037E24EF07DB7506F02FFED1C148C0 (IOSBridgeTester_t50928DFB95A193CA8D2314BF665DF15D2A437ABE* __this, const RuntimeMethod* method) 
+{
+	static bool s_Il2CppMethodInitialized;
+	if (!s_Il2CppMethodInitialized)
+	{
+		il2cpp_codegen_initialize_runtime_metadata((uintptr_t*)&_stringLiteralE87E01CC027DFCFE2D69045A36DFD65E43CE6D57);
+		s_Il2CppMethodInitialized = true;
+	}
+	{
+		// IOSWifiDetails_runNativeCode("CALLED FROM UNITY!");
+		IOSBridgeTester_IOSWifiDetails_runNativeCode_mBE4F9DD95610BB6F54978D965676DC521AB21162(_stringLiteralE87E01CC027DFCFE2D69045A36DFD65E43CE6D57, NULL);
+		// }
+		return;
+	}
+}
+// System.Void IOSBridgeTester::.ctor()
+IL2CPP_EXTERN_C IL2CPP_METHOD_ATTR void IOSBridgeTester__ctor_m0156A96E202D78C413C46C3237A7333A41236F99 (IOSBridgeTester_t50928DFB95A193CA8D2314BF665DF15D2A437ABE* __this, const RuntimeMethod* method) 
+{
+	{
+		MonoBehaviour__ctor_m592DB0105CA0BC97AA1C5F4AD27B12D68A3B7C1E(__this, NULL);
+		return;
+	}
+}
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
---
+++ Build/Classes/Native/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,53 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void IOSBridgeTester::IOSWifiDetails_runNativeCode(System.String)
+extern void IOSBridgeTester_IOSWifiDetails_runNativeCode_mBE4F9DD95610BB6F54978D965676DC521AB21162 (void);
+// 0x00000002 System.Void IOSBridgeTester::Start()
+extern void IOSBridgeTester_Start_m52FB8350EC037E24EF07DB7506F02FFED1C148C0 (void);
+// 0x00000003 System.Void IOSBridgeTester::.ctor()
+extern void IOSBridgeTester__ctor_m0156A96E202D78C413C46C3237A7333A41236F99 (void);
+static Il2CppMethodPointer s_methodPointers[3] = 
+{
+	IOSBridgeTester_IOSWifiDetails_runNativeCode_mBE4F9DD95610BB6F54978D965676DC521AB21162,
+	IOSBridgeTester_Start_m52FB8350EC037E24EF07DB7506F02FFED1C148C0,
+	IOSBridgeTester__ctor_m0156A96E202D78C413C46C3237A7333A41236F99,
+};
+static const int32_t s_InvokerIndices[3] = 
+{
+	1842,
+	973,
+	973,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	3,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
