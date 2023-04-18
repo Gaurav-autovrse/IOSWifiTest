3d7373dac54a849ca413c617a06f4b3f1af61573
Gaurav-autovrse <gaurav.muglikar@autovrse.in>
2023-04-18 18:26:58 +0530

    added builds for both ios simulator and  device

---
+++ Build/IphoneOS/Classes/Native/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,53 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Int32 IOSBridgeTester::printFromNativeCode()
+extern void IOSBridgeTester_printFromNativeCode_m45E2416C8FB8BB765B70B76291FCF9922A128F1E (void);
+// 0x00000002 System.Void IOSBridgeTester::Start()
+extern void IOSBridgeTester_Start_m52FB8350EC037E24EF07DB7506F02FFED1C148C0 (void);
+// 0x00000003 System.Void IOSBridgeTester::.ctor()
+extern void IOSBridgeTester__ctor_m0156A96E202D78C413C46C3237A7333A41236F99 (void);
+static Il2CppMethodPointer s_methodPointers[3] = 
+{
+	IOSBridgeTester_printFromNativeCode_m45E2416C8FB8BB765B70B76291FCF9922A128F1E,
+	IOSBridgeTester_Start_m52FB8350EC037E24EF07DB7506F02FFED1C148C0,
+	IOSBridgeTester__ctor_m0156A96E202D78C413C46C3237A7333A41236F99,
+};
+static const int32_t s_InvokerIndices[3] = 
+{
+	5192,
+	3465,
+	3465,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	3,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
---
+++ Build/IphoneSimulator/Frameworks/Plugins/iOS/NativeIOSFramework.framework/Headers/NativeIOSFramework.h
@@ -0,0 +1,18 @@
+//
+//  NativeIOSFramework.h
+//  NativeIOSFramework
+//
+//  Created by user238009 on 4/17/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NativeIOSFramework.
+FOUNDATION_EXPORT double NativeIOSFrameworkVersionNumber;
+
+//! Project version string for NativeIOSFramework.
+FOUNDATION_EXPORT const unsigned char NativeIOSFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NativeIOSFramework/PublicHeader.h>
+
+
